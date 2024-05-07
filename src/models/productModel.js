2fa3211379fede640c4e71b44d08ab0296c75196
Kashyap297 <kashyap29700@gmail.com>
2024-05-07 23:05:23 +0530

    product schema update

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -6,6 +6,18 @@ const productSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    // description: {
+    //     type : String,
+    //     required : true,
+    // },
+    // price : {
+    //     type : Number,
+    //     required : true
+    // },
+    // productimage : {
+    //     type : String,
+    //     required : true
+    // },
     subCategoryID: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'subcategories'
