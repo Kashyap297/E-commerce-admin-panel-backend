2e98afbb17625b857266771b35c57a081426d883
Kashyap297 <kashyap29700@gmail.com>
2024-05-11 23:05:20 +0530

    crud of manager with ui done

--- src/controllers/managerController.js
+++ src/controllers/managerController.js
@@ -3,8 +3,60 @@ const managerModel = require("../models/managerModel")
 const managerController = {
     create: async (req, res) => {
         try {
-            const manager = await managerModel.create(req.body)
-            res.send(manager)
+            const { name, email, password } = req.body
+            const manager = await managerModel.findOne({ email })
+            if (manager) {
+                return res.status(400).json({
+                    message: 'Email already Exist',
+                    success: false
+                })
+            }
+            const data = await managerModel.create({ name, email, password })
+            res.redirect('/manager')
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    get: async (req, res) => {
+        try {
+            const manager = await managerModel.find()
+            // res.send(manager)
+            res.render('Pages/manager/manager', { managers: manager })
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    form: (req, res) => {
+        try {
+            res.render('Pages/manager/addmanager')
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    delete: async (req, res) => {
+        const { id } = req.params
+        try {
+            const manager = await managerModel.findByIdAndDelete(id);
+            res.redirect('/manager')
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    edit: async (req, res) => {
+        const { id } = req.params
+        try {
+            const manager = await managerModel.findById(id)
+            res.render('Pages/manager/editmanager', { manager: manager })
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    update: async (req, res) => {
+        const { id } = req.params
+        const { name, email, password } = req.body
+        try {
+            const manager = await managerModel.findByIdAndUpdate(id, { name: name, email: email, password: password }, { new: true })
+            res.redirect('/manager')
         } catch (error) {
             console.log(error)
         }
--- src/routes/managerRoute.js
+++ src/routes/managerRoute.js
@@ -4,5 +4,10 @@ const managerController = require('../controllers/managerController')
 const managerRouter = Router()
 
 managerRouter.post('/create', managerController.create)
+managerRouter.get('/', managerController.get)
+managerRouter.get('/create', managerController.form)
+managerRouter.get('/delete/:id', managerController.delete)
+managerRouter.get('/edit/:id', managerController.edit)
+managerRouter.post('/edit/:id', managerController.update)
 
 module.exports = managerRouter
